dbb96ef011b3fae4d5d733ab711084681d7742e0
cscho <cscho0415@gmail.com>
2016-04-19 00:07:36 -0400

    Update FrameSegmenter_RGBLed.m
    This function now takes 2 inputs, file location (needs to be changed to just file) and number of expected dots to find. It now returns the centroid of the found pattern (successfully).
    Also, now more robust: uses images size to calculate certain values rather than having hard coded values.

--- FrameSegmenter_RGBLed.m
+++ FrameSegmenter_RGBLed.m
@@ -1,33 +1,50 @@
-function hulls = FrameSegmenter_RGBLed(im)
+function Centroid = FrameSegmenter_RGBLed(im, expected)
 im = mat2gray(imread(im));
-BW = logical(0*im);
+im = -im(:,:,1)+im(:,:,2)+im(:,:,3);
 % red = im(:,:,1);
 % green = im(:,:,2);
 % blue = im(:,:,3);
 s = size(im);
-for i = 1:s(3)
-    seg = channelSegmenter(im(:,:,i));
-    % for j = min(min(seg))+1:max(max(seg))
-    j = max(max(seg));
+BW = logical(0*im);
+seg = channelSegmenter(im);%(:,:,i));
+%expected = 4;
+for j = min(min(seg))+1:max(max(seg))
     bw = seg==j;
     bw = bwareafilt(bw,[0.00005*numel(bw) .01*numel(bw)]);
     [L num] = bwlabel(bw);
     props = regionprops(L, 'Eccentricity', 'Centroid','Area','ConvexArea');
     for k = 1:num
-        if props(k).Eccentricity >=.7 || props(k).Area/props(k).ConvexArea <.8
+        %get rid of unwanted slop
+        if props(k).Eccentricity >=.5 || props(k).Area/props(k).ConvexArea <.8
             L(L==k)=0;
         end
     end
-    % end
+    BW = logical(min(1, BW+L));
 end
-
-
+[L num] = bwlabel(BW);
+count = num;
+while(num > count - (expected-1)) && (count > expected)
+    BW = imdilate(BW, strel('disk', 1));
+    [L num] = bwlabel(BW);        
+    % solution has been found
+end
+props = regionprops(BW, 'Eccentricity','Centroid','Area');
+m = find([props.Eccentricity]==max([props.Eccentricity]));
+Centroid = props(m).Centroid;
+imagesc(BW);hold on
+plot(Centroid(1),Centroid(2), '+r'); hold off
 end
 
 function seg = channelSegmenter(im)
-filt = fspecial('average',[11 11]);
+s = size(im);
+kx = 0.0052*s(1);
+ky = 0.0052*s(2);
+ksx = 2.*round((kx+1)/2)-1;
+ksy = 2.*round((ky+1)/2)-1;
+filt = fspecial('average',[ksy ksx]);
+im = medfilt2(im, [ksy ksx]);
 avg = imfilter(im, filt);
 th = multithresh(avg,3);
 seg = imquantize(avg,th);
-seg = imclose(seg, strel('disk',20));
+seg = imdilate(seg, strel('disk',floor(1.8e-05*s(1)*s(2))));
 end
