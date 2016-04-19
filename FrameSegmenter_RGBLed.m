cd816e119aed2f452ec1ccd9fae0057efdc2604e
cscho <cscho0415@gmail.com>
2016-04-19 02:53:48 -0400

    Update FrameSegmenter_RGBLed.m
    New parameter, instead of expected number of dots, dot color pattern. From left to right in image [ [1;0;0] [0;0;1] ... [0;0;0]] where each [x;y;z] is the color of that respective dot. Color codes are [red;green;blue] are are {0,1}. For more info on color codes look at http://www.mathworks.com/help/matlab/ref/colorspec.html
    
    Code has been improved for reliability and has been simplified for readability (reduction of loops and if-statements). Comments have been expanded upon, code should now be rather self explanatory.

--- FrameSegmenter_RGBLed.m
+++ FrameSegmenter_RGBLed.m
@@ -1,49 +1,96 @@
-function Centroid = FrameSegmenter_RGBLed(im, expected)
-im = mat2gray(imread(im));
-im = -im(:,:,1)+im(:,:,2)+im(:,:,3);
-% red = im(:,:,1);
-% green = im(:,:,2);
-% blue = im(:,:,3);
-s = size(im);
-BW = logical(0*im);
-seg = channelSegmenter(im);%(:,:,i));
-%expected = 4;
+%% Parameters: image file location, expected number of 'dots' in pattern
+% function Centroid = FrameSegmenter_RGBLed(imp, [pattern])
+function Centroid = FrameSegmenter_RGBLed(imp, pattern)
+expected = size(pattern);
+expected = expected(2);
+
+close all
+% read image, then calculate g+b+1-r image (easiet for finding dots)
+img = mat2gray(imread(imp));
+red = img(:,:,1);
+green = img(:,:,2);
+blue = img(:,:,3);
+im = mat2gray((1-img(:,:,1))+img(:,:,2)+img(:,:,3));%idk, i need to find something better
+zim = logical(0*im);
+BW=zim;
+seg = channelSegmenter(im);
+
+% Loop through each of layer of the trinomial image and remove non circular
+% components
 for j = min(min(seg))+1:max(max(seg))
     bw = seg==j;
     bw = bwareafilt(bw,[0.00005*numel(bw) .01*numel(bw)]);
     [L num] = bwlabel(bw);
-    props = regionprops(L, 'Eccentricity', 'Centroid','Area','ConvexArea');
-    for k = 1:num
-        %get rid of unwanted slop
-        if props(k).Eccentricity >=.5 || props(k).Area/props(k).ConvexArea <.8
-            L(L==k)=0;
+    MaskProps = regionprops(L, 'Eccentricity','Area','ConvexArea');
+    for i = 1:num
+        if MaskProps(i).Eccentricity >=.5 || MaskProps(i).Area/MaskProps(i).ConvexArea <.8
+            L(L==i)=0; %remove components
         end
     end
     BW = logical(min(1, BW+L));
 end
-[L num] = bwlabel(BW);
-count = num;
-while(num > count - (expected-1)) && (count > expected)
-    BW = imdilate(BW, strel('disk', 1));
-    [L num] = bwlabel(BW);        
-    % solution has been found
-end
-props = regionprops(BW, 'Eccentricity','Centroid','Area');
-m = find([props.Eccentricity]==max([props.Eccentricity]));
-Centroid = props(m).Centroid;
-imagesc(BW);hold on
-plot(Centroid(1),Centroid(2), '+r'); hold off
-end
 
+% Dots are close together, dilate till they touch until an eccentric object
+% masks the desired pattern
+CC = bwconncomp(BW);
+BwMask = BW;
+count = CC.NumObjects;
+num = count;
+while(num >= count - (expected-1)) && (count > expected)
+    BwMask = imdilate(BwMask, strel('disk', 2));% higher values are quicker but less resolute
+    CCM = bwconncomp(BwMask);
+    num = CCM.NumObjects;
+    % If/When the number of objects decrease to starting count-number of
+    % expected objects-1 (3 objects becomes 1 -> expected-1)
+    if (num <= count - (expected-1))
+        MaskProps = regionprops(BwMask, 'Eccentricity','Centroid','Area','PixelIdx');
+        % Looping through each mask object, checking if it covers pattern
+        for j=1:numel(MaskProps)
+            if MaskProps(j).Eccentricity < (1-1/expected)
+                continue
+            end
+            prod = zim;
+            prod([MaskProps(j).PixelIdxList])=1;
+            [L, nnum] = bwlabel(prod.*BW);
+            if nnum ~= expected
+                continue;
+            end;
+            % Check for color pattern
+            for k = 1:nnum
+                color = [sum(sum(red(L==k))); sum(sum(green(L==k))); sum(sum(blue(L==k)))];
+                color = round(mat2gray(color))
+                if ~isequal(pattern(:,k), color)
+                    break;
+                elseif k == expected
+                        Centroid = MaskProps(j).Centroid;
+%                         figure
+%                         imagesc(BWD);hold on
+%                         plot(Centroid(1),Centroid(2), '+r','linewidth',3); hold off
+                        return;
+                end;
+            end;
+        end;
+    end;
+    % Pattern was not found, continue dilation to join more dots and create
+    % a bigger mask
+    num = count - (expected-1);
+end
+end%timeout?
+
+
+%creates trinay image (dark, medium, birght) -> (1,2,3)
 function seg = channelSegmenter(im)
+% kernal size calculations
 s = size(im);
 kx = 0.0052*s(1);
 ky = 0.0052*s(2);
 ksx = 2.*round((kx+1)/2)-1;
 ksy = 2.*round((ky+1)/2)-1;
+% Image filtration, median filt then average filt
 filt = fspecial('average',[ksy ksx]);
 im = medfilt2(im, [ksy ksx]);
 avg = imfilter(im, filt);
+% Create trinary image
 th = multithresh(avg,3);
 seg = imquantize(avg,th);
 seg = imdilate(seg, strel('disk',floor(1.8e-05*s(1)*s(2))));
