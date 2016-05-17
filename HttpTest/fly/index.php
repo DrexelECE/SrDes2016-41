ec44b6a15785b27a26c4a2e7112b50db8672006e
James Kurtz <jkrrv@jkrrv.org>
2016-05-17 07:35:24 -0400

    General updates

--- HttpTest/fly/index.php
+++ HttpTest/fly/index.php
@@ -11,3 +11,5 @@ foreach($q as $k => $v) {
 header('Content-Type: application/json');
 
 echo json_encode($set);
+
+echo "\r\n";
--- HttpTest/image/index.php
+++ HttpTest/image/index.php
@@ -1,5 +1,9 @@
 <?php
 
 header("Content-Type: image/jpeg");
+header("Content-Length: " . (string)filesize('image.jpg'));
+
 
 echo file_get_contents('image.jpg');
+
+echo "\r\n"; //\r\n";
